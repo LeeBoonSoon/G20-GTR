98c1873f2093da4f4d2de086c0026c24d6a8cfbb
blank-lcb <103558177+blank-lcb@users.noreply.github.com>
2024-04-08 21:03:25 +0800

    Add files via upload
    Design only

---
+++ NewJava/UploadBasicSettings.java
@@ -0,0 +1,13 @@
+package com.example.educationapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UploadBasicSettings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload_basic_settings);
+    }
+}
