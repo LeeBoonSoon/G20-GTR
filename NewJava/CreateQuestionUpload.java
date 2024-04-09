d74ee0d8478f02cbcbc593c258f02b4389321f9e
blank-lcb <103558177+blank-lcb@users.noreply.github.com>
2024-04-10 04:23:50 +0800

    Almost done

--- NewJava/CreateQuestionUpload.java
+++ NewJava/CreateQuestionUpload.java
@@ -2,13 +2,172 @@ package com.example.educationapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.style.UpdateLayout;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.google.firebase.Firebase;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.example.educationapplication.QuizModel;
+import com.example.educationapplication.QuestionModel;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 
 public class CreateQuestionUpload extends AppCompatActivity {
+
+
+
+    DatabaseReference databaseRef;
+    String Title, Subtitle, Time, ID, ValueA, ValueB, ValueC, ValueD, CorrectAnswer,Question;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_question_upload);
+        FirebaseApp.initializeApp(this);
+
+        SharedPreferences sharedPreferences = getSharedPreferences("Globaldatastore", 0);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        Button uploadbutton = findViewById(R.id.add_submit_btn);
+        Button clearbutton = findViewById(R.id.add_next_btn);
+        //getText.toString()
+        EditText title = findViewById(R.id.add_question_textview);
+        EditText AnswerA = findViewById(R.id.add_answerA);
+        EditText AnswerB = findViewById(R.id.add_answerB);
+        EditText AnswerC = findViewById(R.id.add_answerC);
+        EditText AnswerD = findViewById(R.id.add_answerD);
+        Spinner spinner = findViewById(R.id.addspinner);
+
+        //*****************************************
+        List<QuestionModel> questionList = new ArrayList<>();
+
+        Title = sharedPreferences.getString("Title", "Null");
+        Subtitle = sharedPreferences.getString("Subtitle", "Null");
+        Time = sharedPreferences.getString("Time", "Null");
+        ID = sharedPreferences.getString("ID", "Null");
+
+        //To avoid update crashing ;
+        Question = "";
+        ValueA = "";
+        ValueB = "";
+        ValueC = "";
+        ValueD = "";
+        CorrectAnswer = "";
+
+        //Database
+
+        //Get the question to into arrary
+        clearbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Question = title.getText().toString();
+                ValueA = AnswerA.getText().toString();
+                ValueB = AnswerB.getText().toString();
+                ValueC = AnswerC.getText().toString();
+                ValueD = AnswerD.getText().toString();
+
+                //Question
+                QuestionModel questionModel = new QuestionModel(
+                        Question, Arrays.asList(ValueA, ValueB, ValueC, ValueD), CorrectAnswer
+                );
+                //Add to list of question
+                questionList.add(questionModel); // Add existing question
+
+                Question = "";
+                ValueA = "";
+                ValueB = "";
+                ValueC = "";
+                ValueD = "";
+                CorrectAnswer = "";
+
+                title.setText("Input the Question");
+                AnswerA.setText("AnswerA");
+                AnswerB.setText("AnswerB");
+                AnswerC.setText("AnswerC");
+                AnswerD.setText("AnswerD");
+            }
+        });
+
+        uploadbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Question = title.getText().toString();
+                ValueA = AnswerA.getText().toString();
+                ValueB = AnswerB.getText().toString();
+                ValueC = AnswerC.getText().toString();
+                ValueD = AnswerD.getText().toString();
+
+                //Question
+                QuestionModel questionModel = new QuestionModel(
+                        Question, Arrays.asList(ValueA, ValueB, ValueC, ValueD), CorrectAnswer
+                );
+                //Add to list of question
+                questionList.add(questionModel); // Add existing question
+
+                QuizModel quz = new QuizModel(
+                        ID,
+                        Title,
+                        Subtitle,
+                        Time,
+                        questionList
+                );
+
+                //Get number
+                databaseRef = FirebaseDatabase.getInstance().getReference("5");
+                databaseRef.setValue(quz);
+            }
+        });
+
+        //sprineer
+        String[] options = {"Select the correct answer", "A", "B", "C"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, options);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedOption = options[position];
+                //Change to the input Valid of user
+                options[0] = AnswerA.getText().toString();
+                options[1] = AnswerB.getText().toString();
+                options[2] = AnswerC.getText().toString();
+                options[3] = AnswerD.getText().toString();
+
+                if (position == 0)
+                    CorrectAnswer =  AnswerA.getText().toString();
+                else  if (position == 1)
+                    CorrectAnswer =  AnswerB.getText().toString();
+                else if (position == 2)
+                    CorrectAnswer =  AnswerC.getText().toString();
+                else if (position == 3)
+                    CorrectAnswer =  AnswerD.getText().toString();
+                else
+                    CorrectAnswer = "";
+            }
+
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
     }
+
 }
--- NewJava/UploadBasicSettings.java
+++ NewJava/UploadBasicSettings.java
@@ -1,13 +1,49 @@
 package com.example.educationapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class UploadBasicSettings extends AppCompatActivity {
+
+    //store into local to uisng other java call
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload_basic_settings);
+
+        SharedPreferences sharedPreferences = getSharedPreferences("Globaldatastore", 0);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        EditText addtitle = findViewById(R.id.add_edit_title);
+        EditText addsubtitile = findViewById(R.id.add_edit_subtitle);
+        EditText addtime = findViewById(R.id.add_edit_time);
+        EditText addid = findViewById(R.id.add_edit_id);
+        Button createexam = findViewById(R.id.editsubmitbotton);
+
+        createexam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editor.putString("Title",addtitle.getText().toString());
+                editor.putString("Subtitle",addsubtitile.getText().toString());
+                editor.putString("ID",addid.getText().toString());
+                editor.putString("Time",addtime.getText().toString());
+                editor.apply();
+
+                Intent intent = new Intent(UploadBasicSettings.this,CreateQuestionUpload.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
